79fd79428589b2bdb063d26cfbcdcf7cd15c53ad
futureFAANGdeveloper <yulbakhtintimofei@gmail.com>
2025-04-12 21:13:15 +0500

    brand new script.js

--- script.js
+++ script.js
@@ -1,32 +1,91 @@
-"use strict";
-// Добавляем обработчик события "click" на кнопку с id "addTaskButton"
-document.getElementById("addTaskButton").addEventListener("click", function () {
-  const taskInput = document.getElementById("taskInput");
-  const taskText = taskInput.value.trim();
+const todoForm = document.querySelector(".form");
+const todoInput = document.getElementById("todo-input");
+const todoListUL = document.getElementById("todo-list");
 
-  if (taskText) {
-    const taskList = document.getElementById("taskList");
+let allTodos = getTodos();
+updateTodoList();
 
-    const li = document.createElement("li");
-
-    const span = document.createElement("span");
-    span.textContent = taskText;
-    li.appendChild(span);
-
-    // Создаем кнопку для удаления задачи
-    const deleteButton = document.createElement("button");
-    deleteButton.textContent = "Удалить";
-    deleteButton.className = "delete-button";
-    // Добавляем обработчик события "click" для удаления задачи
-    deleteButton.addEventListener("click", function () {
-      taskList.removeChild(li);
-    });
-
-    // Добавляем кнопку удаления в элемент списка li
-    li.appendChild(deleteButton);
-    taskList.appendChild(li);
-
-    taskInput.value = ""; // Очистить поле ввода
-  }
+todoForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  addTodo();
 });
+function addTodo() {
+  const todoText = todoInput.value.trim();
+  if (todoText.length) {
+    const todoObject = {
+      text: todoText,
+      completed: false,
+    };
+    allTodos.push(todoObject);
+    updateTodoList();
+    saveTodos();
+    todoInput.value = "";
+  }
+}
+function updateTodoList() {
+  todoListUL.innerHTML = "";
+  allTodos.forEach((todo, todoIndex) => {
+    const todoItem = createTodoItem(todo, todoIndex);
+    todoListUL.append(todoItem);
+  });
+}
+function createTodoItem(todo, todoIndex) {
+  const todoId = "todo-" + todoIndex;
+  const todoLI = document.createElement("li");
+  const todoText = todo.text;
+  todoLI.className = "todo__list-item";
+  todoLI.innerHTML = `
+  <input type="checkbox" id="${todoId}" />
 
+          <label for="${todoId}" class="custom-checkbox">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 -960 960 960"
+              width="24px"
+              fill="transparent"
+            >
+              <path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z" />
+            </svg>
+          </label>
+          <label for="${todoId}" class="todo__text"
+            >${todoText}</label
+          >
+          <button class="delete-btn">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 -960 960 960"
+              width="24px"
+              fill="#00ffc4"
+            >
+              <path
+                d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"
+              />
+            </svg>
+          </button>
+  `;
+  const deleteBtn = todoLI.querySelector(".delete-btn");
+  deleteBtn.addEventListener("click", () => {
+    deleteTodoItem(todoIndex);
+  });
+  const checkbox = todoLI.querySelector("input");
+  checkbox.addEventListener("change", () => {
+    allTodos[todoIndex].completed = checkbox.checked;
+    saveTodos();
+  });
+  checkbox.checked = todo.completed;
+  return todoLI;
+}
+function deleteTodoItem(todoIndex) {
+  allTodos = allTodos.filter((_, i) => i !== todoIndex);
+  saveTodos();
+  updateTodoList();
+}
+function saveTodos() {
+  localStorage.setItem("todos", JSON.stringify(allTodos));
+}
+function getTodos() {
+  const todos = localStorage.getItem("todos") || "[]";
+  return JSON.parse(todos);
+}
